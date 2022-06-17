2195abeee96388d8d431d1868ee79660059de27a
MrRiahi <mohammad.r.riahi@gmail.com>
2022-06-17 14:06:42 +0430

    Refactor directories

--- Python-Programming-Basics/Exercise/Exercise1.py
+++ Python-Programming-Basics/Exercise/Exercise1.py
@@ -1,13 +1,13 @@
 ## Multiplication of two matrix
 
 matrix1 = [[1, 2],
-               [3, 4]]
+          [3, 4]]
 
 matrix2 = [[1, 0],
-               [0, 1]]
+          [0, 1]]
 
 matrix = [[0, 0],
-              [0, 0]]
+         [0, 0]]
 
 ## iterate through rows of matrix1
 for i in range(len(matrix1)):
@@ -15,6 +15,9 @@ for i in range(len(matrix1)):
     for j in range(len(matrix2[0])):
         ## iterate through rows of matrix2
         for k in range(len(matrix2)):
-            matrix[i][j] += matrix1[i][k]*matrix2[k][j]
+            matrix[i][j] += matrix1[i][k] * matrix2[k][j]
 
 print(matrix)
+
+
+## Use nested comprehension list
